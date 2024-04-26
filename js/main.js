10f12f0b072c17d641b2b804b2a90fb03bdd405a
Mukola2 <janaon333@gmail.com>
2024-04-26 13:16:39 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(function () {
+  $(".slider__container").slick({
+    centerMode: true,
+    centerPadding: "60px",
+    arrows: false,
+    slidesToShow: 3,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: "40px",
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: "40px",
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+});
