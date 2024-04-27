bfedf0c17ed1af7d48e2f0b38f89df8ec69ee3be
Mikola <das19901990a@gmail.com>
2024-04-27 17:25:56 +0300

    footer

--- js/main.js
+++ js/main.js
@@ -1,26 +1,24 @@
 $(function () {
   $(".slider__container").slick({
-    centerMode: true,
-    centerPadding: "60px",
-    arrows: false,
-    slidesToShow: 3,
     dots: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
     responsive: [
       {
         breakpoint: 768,
         settings: {
           arrows: false,
-          centerMode: true,
-          centerPadding: "40px",
-          slidesToShow: 3,
+
+          slidesToShow: 2,
         },
       },
       {
         breakpoint: 480,
         settings: {
           arrows: false,
-          centerMode: true,
-          centerPadding: "40px",
+
           slidesToShow: 1,
         },
       },
